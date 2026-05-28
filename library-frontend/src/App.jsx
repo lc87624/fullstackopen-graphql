30146d61a87191f18d42ecbfdf2d51ac9c4ba644
lc87624 <lc87624@gmail.com>
2026-05-28 15:45:53 +0800

    Add GraphQL subscription for book additions and update Apollo client setup

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -4,12 +4,29 @@ import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Recommendations from './components/Recommendations'
-import { useApolloClient } from '@apollo/client/react'
+import { useApolloClient, useSubscription } from '@apollo/client/react'
+import { BOOK_ADDED } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [ token, setToken ] = useState(localStorage.getItem('library-user-token'))
+  const [notification, setNotification] = useState(null)
   const client = useApolloClient()
+
+  useSubscription(BOOK_ADDED, {
+    onData: ({ data, client }) => {
+      const addedBook = data.data?.bookAdded
+
+      if (addedBook) {
+        setNotification(`New book added: ${addedBook.title}`)
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
+      }
+
+      client.refetchQueries({ include: ['allBooks'] })
+    }
+  })
 
   const logout = () => {
     setToken(null)
@@ -20,6 +37,8 @@ const App = () => {
 
   return (
     <div>
+      {notification && <div>{notification}</div>}
+
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client'
 import { SetContextLink } from '@apollo/client/link/context'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { ApolloProvider } from '@apollo/client/react'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { createClient } from 'graphql-ws'
 
 const authLink = new SetContextLink((_, { headers }) => {
   const token = localStorage.getItem('library-user-token')
@@ -15,10 +18,28 @@ const authLink = new SetContextLink((_, { headers }) => {
   }
 })
 
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:4000',
+}))
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(new HttpLink({
-    uri: 'http://localhost:4000',
-  })),
+  link: splitLink,
   cache: new InMemoryCache()
 })
 
